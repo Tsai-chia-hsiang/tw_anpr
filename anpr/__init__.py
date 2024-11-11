b78e0b7b7a4a609c0dc65e67fe01834706d0e75a
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2024-11-11 16:39:18 +0800

    adding cer as global for the package

--- anpr/__init__.py
+++ anpr/__init__.py
@@ -1,3 +1,3 @@
-from .ocr import LicensePlate_OCR, character_eval
+from .ocr import LicensePlate_OCR, cer
 from .lp_detector import Veh_Detector_with_LP_detection
 from .lp_detector import LicensePlate_Detector
