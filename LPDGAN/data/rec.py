8f5b6a6fd37c6d6a5b08848d14dfb2602723cf4d
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2024-11-25 21:19:27 +0800

    adding paddleocr result

--- LPDGAN/data/rec.py
+++ LPDGAN/data/rec.py
@@ -7,6 +7,7 @@ import logging
 from paddleocr.ppocr.utils.logging import get_logger
 import easyocr
 from torchvision.transforms import transforms, ToTensor, Normalize
+from .aug import normalize_brightness
 
 _paddle_logger = get_logger()
 _paddle_logger.setLevel(logging.ERROR)
@@ -20,28 +21,14 @@ __all__ = ["EXT_MAP"]
 def paddle_text_info(img:Path, on_size:tuple[int, int]=(224, 112)) -> torch.Tensor:
     global _paddleocr_model_         
     
-    def count_area(d)->int:    
-        if d is None:
-            return 0
-        return (d[0][1][0] - d[0][0][0])*(d[0][2][1] - d[0][0][1])
-
-
+   
     if _paddleocr_model_ is None:
         _paddleocr_model_ = PaddleOCR(use_angle_cls=True, lang="en")
  
-    i = cv2.resize(cv2.imread(img), on_size)
-    result = _paddleocr_model_.ocr(i, cls=True)[0]
+    i = normalize_brightness(cv2.resize(cv2.imread(img), on_size))
+    logit = _paddleocr_model_.ocr(i, cls=True, det=False)[0][0][3]
+    return torch.from_numpy(logit)
     
-    if result is None:
-        return []
-    
-    main_patch = np.argmax(np.array([count_area(r) for r in result]))
-    t = torch.from_numpy(result[main_patch][1][2])
-    if len(t) < 10:
-        t = torch.cat([t, torch.zeros(10-len(t))])
-    elif len(t)>10:
-        raise ValueError(f"{img} in paddle OCR get too long region")
-    return t
 
 def get_easy_ocr_rcnn():
     reader = easyocr.Reader(['en'], gpu=False)  # You can set gpu=True if you have a GPU
