7be9f8635edc44ab5988950ebdce6913fa46a565
Tsai-chia-hsiang <tsaichiahsiang@gmail.com>
2024-11-27 00:06:32 +0800

    to merge

--- train.sh
+++ train.sh
@@ -3,29 +3,31 @@ TXT_EXT=paddleocr
 TXT_CACHED=$DATA_ROOT/$1/paddleocr.pth
 VAL_DATA_ROOT=./dataset/labeled/
 LABEL_FILE=./dataset/labeled/北鎮所_labels.json
-TRAIN_BATCH=15
-VAL_BATCH=15
-N_EPOCHS=100
-#PRETRAIN=./LPDGAN/checkpoints/old_dataset/latest
-#CKPT=./LPDGAN/checkpoints/old_dataset/latest
-N_EPOCHS_DECAY=100
+TRAIN_BATCH=196
+VAL_BATCH=196
+SEED=89112
+N_EPOCHS=20
+N_EPOCHS_DECAY=40
 LR=0.0001
 SAVE_ROOT=./LPDGAN/checkpoints
-SAVE_FEQ=5000
-SAVE_EPOCH=5
+SAVE_EPOCH=2
 TRAIN_DATASET=$DATA_ROOT/$1
-GPUID=$2
-SAVE=$SAVE_ROOT/$3
-PLATE_LOSS=$4
-
-python train_LPDGAN.py \
+GPUID=cuda
+SAVE=$SAVE_ROOT/$2
+PLATE_LOSS=probl1
+WARM_UP=2
+PRETRAINED=$SAVE_ROOT/pretrained
+WBATCH=320
+    
+CUDA_VISIBLE_DEVICES=0,1,3,4 python train_LPDGAN.py \
     --data_root $TRAIN_DATASET \
-    --txt_cached $TXT_CACHED --txt_extract $TXT_EXT\
-    --txt_loss $PLATE_LOSS \
+    --txt_cached $TXT_CACHED --txt_extract $TXT_EXT --txt_loss $PLATE_LOSS \
     --val_data_root $VAL_DATA_ROOT  --label_file $LABEL_FILE\
     --gpu_id $GPUID --batch_size $TRAIN_BATCH --val_batch $VAL_BATCH\
     --n_epochs $N_EPOCHS --n_epochs_decay $N_EPOCHS_DECAY \
     --lr $LR --model_save_root $SAVE\
-    --save_epoch_freq $SAVE_EPOCH
-    #--pretrained_weights $PRETRAIN --checkpoint_dir $CKPT \
+    --save_epoch_freq $SAVE_EPOCH\
+    --seed $SEED --preload \
+    --D_warm_up $WARM_UP --warm_up_batch_size $WBATCH \
+    --pretrained_weights $PRETRAINED --load_G \\
     
