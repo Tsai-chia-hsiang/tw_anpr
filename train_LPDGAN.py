4b27bdf1f4f864c168bd224e98bf117aaf961f8f
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2024-11-26 22:41:07 +0800

    modfity this script so that it can be only for epoch based validation and adding D warm-up epochs

--- train_LPDGAN.py
+++ train_LPDGAN.py
@@ -6,20 +6,17 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import shutil
 import random
-from typing import Optional
-from logging import Logger
 import time
-from tqdm import tqdm
+from tqdm import tqdm, trange
 from argparse import ArgumentParser, Namespace
 import torch
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
-import cv2
 import json
 from torch.utils.data import DataLoader
 from pathlib import Path
 from LPDGAN import LP_Deblur_Dataset, LP_Deblur_OCR_Valiation_Dataset, LPDGAN_Trainer, LPDGAN_DEFALUT_CKPT_DIR, LPD_OCR_ACC_Evaluator
-from LPDGAN.logger import get_logger
+from LPDGAN.logger import get_logger, print_infomation
 from imgproc_utils import L_CLAHE
 
 def reproducible(seed:int = 891122):
@@ -35,43 +32,7 @@ def reproducible(seed:int = 891122):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def evaluation_and_save(
-    lpdgan:LPDGAN_Trainer, val_loader:DataLoader, eva:LPD_OCR_ACC_Evaluator, 
-    save_dir:Path, logger:Logger,iters:int, baseline:dict[str, float], 
-    epoch:Optional[int]=None, board:Optional[SummaryWriter]=None,
-    keep_training:bool=True
-) -> float:
-    if val_loader is not None:
-        # Having a validaiton dataset.
-        # Will first validate the model and decide wether to save 
-        val_msg = f"validation at iteration {iters}"
-        if epoch is not None:
-            val_msg += f"(End of epoch {epoch:3d})"
-        
-        logger.info(val_msg)
-        
-        lpdgan.netG.eval()
-        last_best = eva.current_best
-        acc = eva.val_LP_db_dataset(val_loader=val_loader, swintrans_g=lpdgan.netG)
-        update_status = eva.update(accs=acc)
-        for k in acc:
-            logger.info(f"{k}: baseline: {baseline[k]} | acc : {acc[k]} | last best: {last_best[k]} | {update_status[k]}")
-            if board is not None:
-                board.add_scalars(
-                    k, {"train":acc[k], "baseline":baseline[k]},
-                    iters
-                )
-            if update_status[k] == LPD_OCR_ACC_Evaluator.update_signal:
-                lpdgan.save_networks(save_dir=save_dir/f"{k}")
-        if keep_training:
-            lpdgan.netG.train()
-        
-        return acc
-    else:
-        # no validation, directly save
-        logger.info(f"no validation set, directly save to {save_dir}")
-        lpdgan.save_networks(save_dir=save_dir)
-        return -1.0
+
 
 def main(args:Namespace):
     
@@ -84,7 +45,7 @@ def main(args:Namespace):
     logger = get_logger(name=__name__, file=args.model_save_root/"training.log")
     tensorboard_writer = SummaryWriter(log_dir=args.model_save_root)
     
-    logger.info(f"{args}")
+    print_infomation(f"{args}", logger=logger)
     
     reproducible(seed=args.seed)
     
@@ -117,94 +78,71 @@ def main(args:Namespace):
 
     lpdgan = LPDGAN_Trainer(
         logger=logger, epochs_policy= {
-            'n_epochs':args.n_epochs,
+            'n_epochs':args.n_epochs + args.D_warm_up,
             'n_epochs_decay':args.n_epochs_decay,
             'lr_decay_iters':args.lr_decay_iters
         },
         pretrained_weights=args.pretrained_weights,
-        checkpoint_dir=args.checkpoint_dir, 
         gan_mode=args.gan_mode,
         lr=args.lr, lr_policy=args.lr_policy, lambda_L1=args.lambda_L1, 
         ocr_percepual=args.ocr_perceptual,
         gpu_id=args.gpu_id,
         txt_recons_dim=dataset.txt_d,
-        plate_loss =args.txt_loss
+        plate_loss =args.txt_loss,
+        D_warm_up=args.D_warm_up,
+        load_G=args.load_G, load_D=args.load_D
     )
     
-    total_iters = 0
-    save_flag = 0
     epoch_loss = None
-    for epoch in range(1, args.n_epochs + args.n_epochs_decay + 1):
-        
-        epoch_start_time = time.time()
-        lpdgan.update_learning_rate(logger=logger)
-        bar = tqdm(trainloader)
-
-        epoch_val_flag = (not args.save_iter) and epoch % args.save_epoch_freq == 0
-        
-        for data in bar:
-            n_samples = len(data['A_paths'])
-            total_iters += n_samples
-            lpdgan.optimize_parameters(input_x=data)
-            bar.set_postfix(ordered_dict={"iters":total_iters})
-            iter_val_flag = (args.save_iter) and (total_iters // args.save_iter_freq > save_flag)
+    pbar = trange(1, args.D_warm_up + args.n_epochs + args.n_epochs_decay + 1)
+    for epoch in pbar:
+        epoch_val_flag = (epoch > args.D_warm_up) and (epoch - args.D_warm_up) % args.save_epoch_freq == 0
+        epoch_iters = 0
+        old_lr, lr = args.lr, args.lr 
+        for data in trainloader:
+            n = len(data['A_paths'])
+            epoch_iters += n
+            
+            lpdgan.optimize_parameters(input_x=data, step=epoch-1)
+            pbar.set_postfix(ordered_dict={'iters':f"{epoch_iters}/{len(dataset)}", "stage":lpdgan.stage, "lr":f"{lr}<-{old_lr}"})
             
             if epoch_val_flag:
-                # epoch validation policy 
                 iter_loss = lpdgan.get_current_losses()
                 if epoch_loss is None:
                     epoch_loss = iter_loss.copy()
                 else:
                     for k in iter_loss:
-                        epoch_loss[k] += iter_loss[k]
+                        epoch_loss[k] += iter_loss[k]*n
 
-            if iter_val_flag:
-                # iteration validation policy 
-                iter_loss = lpdgan.get_current_losses()
-                tensorboard_writer.add_scalars("Loss",iter_loss, total_iters)
-                logger.info(f"iters:{total_iters}:{iter_loss}")
-                if args.not_save:
-                    continue
-                evaluation_and_save(
-                    lpdgan=lpdgan, val_loader=val_loader, eva=validator, 
-                    save_dir = args.model_save_root if val_loader is not None \
-                        else args.model_save_root /f'iter_{total_iters}',
-                    iters=total_iters, board=tensorboard_writer,logger=logger,
-                    baseline=baseline
-                )
-                
-                save_flag = total_iters // args.save_iter_freq
+        old_lr, lr = lpdgan.update_learning_rate()
 
         if epoch_val_flag:
-            # epoch validation policy 
+            
             for k in epoch_loss:
                 epoch_loss[k] /= len(dataset)
 
             tensorboard_writer.add_scalars("Loss",epoch_loss, epoch)
-            logger.info(f"epoch:{epoch}:{epoch_loss}")
+            epoch_loss=None
+            gc.collect()
+            print_infomation(f"epoch:{epoch}:{epoch_loss}", logger=logger)
+            
             if args.not_save:
                 continue
-            evaluation_and_save(
-                lpdgan=lpdgan, val_loader=val_loader,
-                eva=validator, 
-                save_dir = args.model_save_root if val_loader is not None \
-                    else args.model_save_root /f'epoch_{epoch}',
-                iters=epoch, board=tensorboard_writer,logger=logger,
-                baseline=baseline, epoch=epoch
-            )
-            epoch_loss = None
-            gc.collect()
-        
-        logger.info(f'End of epoch {epoch} / {args.n_epochs + args.n_epochs_decay}\t Time Taken: {time.time() - epoch_start_time} sec')
-        
-        if not args.not_save_last:
-            lpdgan.save_networks(save_dir = args.model_save_root/'latest')
-            lpdgan.save_optimizers(save_dir = args.model_save_root/'latest')
-            logger.info(f"Last step optimizers and schedulers are saved at { args.model_save_root/'latest'}")
-    
+            
+            if validator is not None:
+                validator(
+                    lpdgan=lpdgan, val_loader=val_loader,
+                    save_dir = args.model_save_root ,
+                    iters=epoch, board=tensorboard_writer,
+                    logger=logger, baseline=baseline
+                )
+            else:
+                epoch_save = args.model_save_root /f'epoch_{epoch}'
+                lpdgan.save_networks(save_dir=epoch_save)
+          
+     
     lpdgan.save_networks(save_dir = args.model_save_root/'latest')
-    lpdgan.save_optimizers(save_dir = args.model_save_root/'latest')
-    logger.info(f"Last step optimizers and schedulers are saved at { args.model_save_root/'latest'}")
+    print_infomation(f"Last step optimizers and schedulers are saved at { args.model_save_root/'latest'}", logger=logger)
 
     if validator is not None:
         with open(args.model_save_root/f"val_metrics.json", "w+") as jf:
@@ -233,21 +171,23 @@ if __name__ == "__main__":
 
     # Batch size & epochs & lr scheduler
     parser.add_argument("--batch_size", type=int, default=40)
+    parser.add_argument('--D_warm_up', type=int, default=2)
     parser.add_argument("--n_epochs", type=int, default=100, help='number of epochs with the initial learning rate')
     parser.add_argument("--n_epochs_decay", type=int, default=200, help='number of epochs to linearly decay learning rate to zero')
     parser.add_argument("--lr_decay_iters", type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
     parser.add_argument('--lr_policy', type=str, default='linear',help='learning rate policy. [linear | step | plateau | cosine]')
-
+    
     # save mode 
     parser.add_argument('--not_save', action='store_true')
     parser.add_argument('--save_epoch_freq', type=int, default=5)
-    parser.add_argument('--save_iter', action='store_true')
-    parser.add_argument('--save_iter_freq', type=int, default=5000)
     parser.add_argument("--model_save_root", type=Path, default=LPDGAN_DEFALUT_CKPT_DIR)
-    parser.add_argument("--not_save_last", action='store_true')
+   
     # load pretrained model
     parser.add_argument('--pretrained_weights', type=Path, default=None)
     parser.add_argument('--checkpoint_dir', type=Path, default=None, help='Using the optimizers and schedulers saved from previous work to keep training')
+    parser.add_argument('--load_G', action='store_true')
+    parser.add_argument('--load_D', action='store_true')
+    
     # lr
     parser.add_argument('--weight_decay', type=float, default=0)
     parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
