0e90859e95f23ce364543dfc4afd7ac33108bf36
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2024-12-01 08:03:32 +0800

    to merge

--- validation.sh
+++ validation.sh
@@ -1,6 +1,10 @@
 DATA_ROOT=./dataset/labeled/
-LABEL=./dataset/labeled/北鎮所_labels.json
-DB_CKPT=$1
-GPUID=$2
-python ocr_eval.py --data_root $DATA_ROOT --label_file $LABEL\
-     --deblur_weight_dir $DB_CKPT --gpu $GPUID
+LABEL=./dataset/labeled/labels.json
+#DB_CKPT=$1
+#GPUID=$2
+python ocr_eval.py --data_root $DATA_ROOT --label_file $LABEL --gpu 0 \
+     --ckpt ./LPDGAN/checkpoints/paddle_ocr_current_best/lcs/net_G.pth
+     # --ckpt ./LPDGAN/checkpoints/old_paddle_please_gogo/lcs/net_G.pth
+     #--ckpt ./LPDGAN/checkpoints/easyocr_new_l1_overfitting/lcs/net_G.pth
+
+     
