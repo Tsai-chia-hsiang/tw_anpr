8d18c4e49fb28cebb82e4370aa6c563187013ca7
Chia-Hsiang Tsai <tsaichiahsiang2000@gmail.com>
2024-12-01 08:10:43 +0800

    merge done

--- train.sh
+++ train.sh
@@ -3,23 +3,23 @@ TXT_EXT=paddleocr
 TXT_CACHED=$DATA_ROOT/$1/paddleocr.pth
 VAL_DATA_ROOT=./dataset/labeled/
 LABEL_FILE=./dataset/labeled/北鎮所_labels.json
-TRAIN_BATCH=196
-VAL_BATCH=196
+TRAIN_BATCH=47
+VAL_BATCH=47
 SEED=89112
-N_EPOCHS=20
-N_EPOCHS_DECAY=40
-LR=0.0001
+N_EPOCHS=100
+N_EPOCHS_DECAY=100
+LR=0.00015
 SAVE_ROOT=./LPDGAN/checkpoints
 SAVE_EPOCH=2
 TRAIN_DATASET=$DATA_ROOT/$1
 GPUID=cuda
 SAVE=$SAVE_ROOT/$2
 PLATE_LOSS=probl1
-WARM_UP=2
-PRETRAINED=$SAVE_ROOT/pretrained
-WBATCH=320
-    
-CUDA_VISIBLE_DEVICES=0,1,3,4 python train_LPDGAN.py \
+WARM_UP=1
+PRETRAINED=./LPDGAN/checkpoints/pretrained
+WBATCH=85
+
+python train_LPDGAN.py \
     --data_root $TRAIN_DATASET \
     --txt_cached $TXT_CACHED --txt_extract $TXT_EXT --txt_loss $PLATE_LOSS \
     --val_data_root $VAL_DATA_ROOT  --label_file $LABEL_FILE\
